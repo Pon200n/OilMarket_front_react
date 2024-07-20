991e685672bfd2ab2bb486ad7b90f7dd1c7e6e87
Dmit <kutsenko.dmitrij2517@yandex.ru>
2024-07-20 16:23:00 +0700

    env server url

--- src/http/index.js
+++ src/http/index.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const $host = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  // baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL_SERVER,
 });
 const $authHost = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  // baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL_SERVER,
 });
 
 const authInterceptor = (config) => {
